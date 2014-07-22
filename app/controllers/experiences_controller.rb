216d389193ad333fd888004e97d35a4467f7eace
gnazarkin <gleb.nazarkin@gmail.com>
2014-07-21 21:54:03 -0400

    Updated experiences controller

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,19 +1,30 @@
 class ExperiencesController < ApplicationController
   def index
+    @experiences = Experience.all
   end
 
   def new
+    @experience = Experience.new
   end
 
   def show
+    @experience = Experience.find(params[:id])
   end
 
   def create
+    @experience = Experience.new(experience_params)
   end
 
   def update
+    @experience = Experience.find(params[:id])
   end
 
   def destroy
+    @experience = Experience.find(params[:id])
+    @experience.destroy
+  end
+
+  private
+  def params.require(:expreience).permit(:name, :description, :start_time, :end_time, :price)  
   end
 end
